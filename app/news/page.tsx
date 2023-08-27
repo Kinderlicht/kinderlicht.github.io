c3799412e47354fd08b22b8cdd92252990e5c675
Matthias Kettl <matthias.kettl@sosy.ifi.lmu.de>
2023-08-28 00:32:44 +0200

    Fix warnings

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="container px-5 py-24 mx-auto max-w-7x1">
         <div className="flex flex-wrap w-full mb-4 p-4">
           <div className="w-full mb-6 lg:mb-0">
-            <h1 className="sm:text-4xl text-5xl font-medium font-bold title-font mb-2 text-gray-900">Unsere Berichte</h1>
+            <h1 className="sm:text-4xl text-5xl font-bold title-font mb-2 text-gray-900">Unsere Berichte</h1>
             <div className="h-1 w-20 bg-indigo-500 rounded"></div>
           </div>
         </div>
