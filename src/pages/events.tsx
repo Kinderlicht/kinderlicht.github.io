009895aa3a55bc46fd81d1113de6afa0e188dc81
Matthias Kettl <matthias.kettl@sosy.ifi.lmu.de>
2024-10-01 13:19:40 +0200

    Always show registering page

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -39,15 +39,13 @@ function EventView(event: Event, index: number) {
       </span>
     );
     finished = true;
-  } else if (
-    eventDate > currentDate &&
-    eventDate.getTime() - currentDate.getTime() < eventIsSoon
-  ) {
+  } else {
     additional = (
       <>
+        {(eventDate.getTime() - currentDate.getTime() < eventIsSoon) &&
         <span className="bg-green-100 text-green-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
           Bevorstehend
-        </span>
+        </span>}
         {event["url"] && (
           <Link
             to={event["url"]}
