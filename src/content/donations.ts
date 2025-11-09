9aa5da320267d99795fac0e9839d62bb99d40095
Matthias Kettl <45742990+matthiaskettl@users.noreply.github.com>
2025-11-09 19:11:53 +0100

    Update donation activities with new entries

--- src/content/donations.ts
+++ src/content/donations.ts
@@ -2,16 +2,100 @@ import { Activity } from "../components/horizontal_scroll";
 
 export const activities: Activity[] = [
     {
-      "date": "2025-01-01T00:00:00.000Z",
+      "date": "2025-08-29T00:00:00.000Z",
+      "donation": 20000,
+      "title": "Hundetherapie",
+      "description": "Familie in Not"
+    },
+    {
+      "date": "2025-08-27T00:00:00.000Z",
+      "donation": 21600,
+      "title": "Vier Deutschlandtickets",
+      "description": "Familie in Not"
+    },
+    {
+      "date": "2025-08-25T00:00:00.000Z",
+      "donation": 34900,
+      "title": "Mobiltelefon für Kind",
+      "description": "Familie in Not"
+    },
+    {
+      "date": "2025-08-20T00:00:00.000Z",
+      "donation": 20000,
+      "title": "Familienausflug Wildpark",
+      "description": "Familie in Not"
+    },
+    {
+      "date": "2025-08-12T00:00:00.000Z",
+      "donation": 45000,
+      "title": "Zuschuss für Tablet",
+      "description": "Digitale Schulklasse"
+    },
+    {
+      "date": "2025-07-30T00:00:00.000Z",
+      "donation": 50000,
+      "title": "Medizinische Kleidung",
+      "description": "Familie in Not"
+    },
+    {
+      "date": "2025-04-01T00:00:00.000Z",
+      "donation": 50000,
+      "title": "Kind unterstützt",
+      "description": "Familie in Not"
+    },
+    {
+      "date": "2025-03-01T00:00:00.000Z",
+      "donation": 10000,
+      "title": "Lebensmittelgutschein",
+      "description": "Familie in Not"
+    },
+    {
+      "date": "2025-03-01T00:00:00.000Z",
+      "donation": 25000,
+      "title": "Zuschuss für Fahrzeug",
+      "description": "Familie in Not"
+    },
+    {
+      "date": "2025-03-01T00:00:00.000Z",
+      "donation": 35000,
+      "title": "Schulfahrt nach Berlin",
+      "description": "Familie in Not"
+    },
+    {
+      "date": "2025-03-01T00:00:00.000Z",
+      "donation": 90000,
+      "title": "Essensgutscheine",
+      "description": "Familie in Not"
+    },
+    {
+      "date": "2025-03-01T00:00:00.000Z",
+      "donation": 11600,
+      "title": "Deutschlandticket",
+      "description": "Familie in Not"
+    },
+    {
+      "date": "2025-01-20T00:00:00.000Z",
+      "donation": 200000,
+      "title": "Mobilitätshilfe",
+      "description": "Familie in Not"
+    },
+    {
+      "date": "2025-01-21T00:00:00.000Z",
       "donation": 11477,
       "title": "Schwimmwesten",
       "description": "Thomas Wiser Haus"
     },
     {
-      "date": "2025-01-01T00:00:00.000Z",
+      "date": "2025-01-20T00:00:00.000Z",
       "donation": 77784,
       "title": "iPad",
       "description": "Digitale Schulklasse"
+    },
+    {
+      "date": "2025-01-13T00:00:00.000Z",
+      "donation": 40395,
+      "title": "Tischtennisplatte",
+      "description": "Thomas Wiser Haus"
     },
     {
       "date": "2024-12-01T00:00:00.000Z",
