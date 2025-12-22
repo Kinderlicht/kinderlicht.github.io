40238645b86e52504f777b37d58d5a742e747063
Matthias Kettl <matthias.kettl@sosy.ifi.lmu.de>
2025-12-22 22:37:02 +0100

    Support svgs

--- src/pages/wir.tsx
+++ src/pages/wir.tsx
@@ -13,12 +13,19 @@ function SponsorCard({ sponsor }: { sponsor: Sponsor }) {
           <div className="p-6">
             <div className="flex flex-wrap items-center">
               <div className="flex-basis shrink-0 grow-0 px-3 w-5/12">
-                {sponsor.image && (
+                {sponsor.image && sponsor.image.extension !== "svg" && (
                   <GatsbyImage
-                    className="mb-6 dark:brightness-150 lg:mb-0 h-24 object-contain"
-                    image={sponsor.image.childImageSharp.gatsbyImageData}
-                    alt={sponsor.name}
-                    objectFit="contain"
+                  className="mb-6 dark:brightness-150 lg:mb-0 h-24 object-contain"
+                  image={sponsor.image.childImageSharp.gatsbyImageData}
+                  alt={sponsor.name}
+                  objectFit="contain"
+                  />
+                )}
+                {sponsor.image && sponsor.image.extension === "svg" && (
+                  <img
+                  src={sponsor.image.publicURL}
+                  alt={sponsor.name}
+                  className="mb-6 dark:brightness-150 lg:mb-0 h-24 object-contain"
                   />
                 )}
                 {sponsor.externalImage && (
